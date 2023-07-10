a7a1d34e41fb8520fce122b1a0eb6a7b45a7488b
Chahyun <ckgus265@gmail.com>
2023-07-10 16:55:03 +0900

    대출심사 프로젝트 커밋

---
+++ fintech/docker/docker_build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Setting Versions
+VERSION='1.0.4'
+cd ..
+./gradlew clean build -x test
+ROOT_PATH=`pwd`
+echo $ROOT_PATH
+
+echo 'API docker Image Build...'
+cd $ROOT_PATH/api && docker build -t api:$VERSION .
+echo 'API docker Image Build... Done'
+
+echo 'CONSUMER docker Image Build...'
+cd $ROOT_PATH/consumer && docker build -t consumer:$VERSION .
+echo 'CONSUMER docker Image Build... Done'
+
+echo 'CSS docker Image Build...'
+cd $ROOT_PATH/css && docker build -t css:$VERSION .
+echo 'CSS docker Image Build... Done'
+
+echo 'nginx docker Image Build...'
+cd $ROOT_PATH/nginx  && docker build -t nginx:$VERSION .
+echo 'nginx docker Image Build... Done'
---
+++ fintech/docker/fintech-redis.sh
@@ -0,0 +1,6 @@
+docker network create fintech-net
+
+docker run --name fintech-redis \
+             -p 6379:6379 \
+             --network fintech-net \
+             -d redis:latest
